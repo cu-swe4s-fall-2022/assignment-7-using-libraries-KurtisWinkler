683cedf7272918b18f74635f46818dc4897896cf
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 02:31:41 +0000

    updated to pass tests

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,16 @@
-# remember to import your libraries!
+import numpy as np
+import pandas as pd
+
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+    
+    if type(num_rows) != int or type(num_columns) != int:
+        raise TypeError("num_rows and num_columns must be integers")
+        
+    if min(num_rows, num_columns) < 1:
+        raise IndexError('num_rows and num_columns must be >= 1')
+
+    return np.random.rand(num_rows, num_columns)
 
 def get_file_dimensions(file_name):
 	return (0,0)
