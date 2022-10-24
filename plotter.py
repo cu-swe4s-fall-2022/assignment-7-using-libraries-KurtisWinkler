76a7d440b1949ebd2ab40cc4497545e11ccc57b2
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 04:45:12 +0000

    update pycodestyle

--- plotter.py
+++ plotter.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 iris = pd.read_csv('iris.data', sep=',', header=None)
-iris.columns = ['sepal_width', 'sepal_length', 'petal_width', 'petal_length', 'species']
+iris.columns = ['sepal_width',
+                'sepal_length',
+                'petal_width',
+                'petal_length',
+                'species']
 
 # make box plot
 fig1, ax1 = plt.subplots()
 params = ['sepal_width', 'sepal_length', 'petal_width', 'petal_length']
-ax1.boxplot(iris[params], labels = params)
+ax1.boxplot(iris[params], labels=params)
 ax1.set_ylabel('cm')
 fig1.savefig('iris_boxplot.png')
 
@@ -15,7 +20,10 @@ fig1.savefig('iris_boxplot.png')
 fig2, ax2 = plt.subplots()
 for species_name in set(iris['species']):
     iris_subset = iris[iris['species'] == species_name]
-    plt.scatter(iris_subset['petal_length'], iris_subset['petal_width'], label=species_name, s=5)
+    plt.scatter(iris_subset['petal_length'],
+                iris_subset['petal_width'],
+                label=species_name,
+                s=5)
 ax2.legend()
 ax2.set_xlabel('petal_length (cm)')
 ax2.set_ylabel('petal_width (cm)')
@@ -30,7 +38,10 @@ axes[0].set_ylabel('cm')
 
 for species_name in set(iris['species']):
     iris_subset = iris[iris['species'] == species_name]
-    axes[1].scatter(iris_subset['petal_length'], iris_subset['petal_width'],label=species_name, s=5)
+    axes[1].scatter(iris_subset['petal_length'],
+                    iris_subset['petal_width'],
+                    label=species_name,
+                    s=5)
 axes[1].legend()
 axes[1].set_xlabel('petal_length (cm)')
 axes[1].set_ylabel('petal_width (cm)')
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('/home/jovyan/assignment-7-using-libraries-KurtisWinkler')
 import data_processor as dp  # nopep8
 
+
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
@@ -14,20 +15,19 @@ class TestUtils(unittest.TestCase):
         for i in range(100):
             f_fixed.write(str([i, i + 1, i + 2]) + '\n')
         f_fixed.close()
-        
+
         self.test_file_rand = 'setup_test_file_rand.txt'
         f_rand = open(self.test_file_rand, 'w')
-        self.rand_num = random.randint(1,100)
+        self.rand_num = random.randint(1, 100)
         for i in range(self.rand_num):
-            rand_int = random.randint(1,100)
+            rand_int = random.randint(1, 100)
             f_rand.write(str([rand_int, rand_int * 2]) + '\n')
         f_rand.close()
 
-        
     def tearDown(self):
         os.remove(self.test_file_fixed)
         os.remove(self.test_file_rand)
-    
+
     # ***Test get_random_matrix***
     def test_error_get_random_matrix(self):
 
@@ -41,39 +41,39 @@ class TestUtils(unittest.TestCase):
     def test_fixed_get_random_matrix(self):
 
         test = np.array([[0.5, 0.5, 0.5],
-                          [0.7, 0.7, 0.7]])
+                         [0.7, 0.7, 0.7]])
         real = dp.get_random_matrix(2, 3)
-        
+
         # positve test
         self.assertEqual(test.shape, real.shape)
-        
+
         # values >= 0real.min()
         self.assertGreaterEqual(real.min(), 0)
-        
+
         # values <= 1
         self.assertLessEqual(real.max(), 1)
-        
+
         # negative test
-        self.assertNotEqual((1,1),real.shape)
-                         
+        self.assertNotEqual((1, 1), real.shape)
+
     def test_random_get_random_matrix(self):
 
-        a = random.randint(1,10)
-        b = random.randint(1,10)
-        real = dp.get_random_matrix(a,b)
-        
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        real = dp.get_random_matrix(a, b)
+
         # positve test
-        self.assertEqual((a,b), real.shape)
-        
+        self.assertEqual((a, b), real.shape)
+
         # values >= 0real.min()
         self.assertGreaterEqual(real.min(), 0)
-        
+
         # values <= 1
         self.assertLessEqual(real.max(), 1)
-        
+
         # negative test
-        self.assertNotEqual((a-1,b), real.shape)
-        
+        self.assertNotEqual((a-1, b), real.shape)
+
     # ***Test get_file_dimensions***
     def test_error_get_file_dimensions(self):
 
@@ -105,7 +105,7 @@ class TestUtils(unittest.TestCase):
 
         # Type error if file_name not string
         self.assertRaises(TypeError, dp.write_matrix_to_file, 5, 5, 5)
-        
+
         # TypeError if input not integer
         self.assertRaises(TypeError, dp.write_matrix_to_file, 2.5, 4, 'a')
         self.assertRaises(TypeError, dp.write_matrix_to_file, 3, 'a', 'a')
@@ -113,7 +113,7 @@ class TestUtils(unittest.TestCase):
     def test_fixed__write_matrix_to_file(self):
 
         real_val = dp.write_matrix_to_file(5, 5, 'a')
-        
+
         # positve test - return true if succeeds
         self.assertEqual(True, real_val)
 
@@ -122,10 +122,10 @@ class TestUtils(unittest.TestCase):
 
     def test_rand__write_matrix_to_file(self):
 
-        a = random.randint(1,10)
-        b = random.randint(1,10)
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
         real_val = dp.write_matrix_to_file(a, b, 'a')
-        
+
         # positve test - return true if succeeds
         self.assertEqual(True, real_val)
 
