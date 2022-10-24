04919ba4986216c16f168022f74e2e83f66cea6c
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 05:13:09 +0000

    updated to process right file

--- tests/func_test.sh
+++ tests/func_test.sh
@@ -3,6 +3,6 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_plotter python3 plotter.py
+run test_plotter python3 ../plotter.py
 assert_no_stdout
 assert_exit_code 0
