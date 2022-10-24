79f743d8d54e9dfd022f2f8338b256ebe1455efd
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 05:01:41 +0000

    added functional tests for plotter.py

---
+++ tests/func_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run test_plotter python3 plotter.py
+assert_no_stdout
+assert_exit_code 0
