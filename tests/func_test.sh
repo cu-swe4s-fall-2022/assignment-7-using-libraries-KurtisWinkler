b7f8bd31476ae35fcb5d3700d5a1c0361d132697
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 05:33:34 +0000

    added new tests

--- tests/func_test.sh
+++ tests/func_test.sh
@@ -3,6 +3,10 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_plotter python3 ../plotter.py
+run test_plotter python3 ../plotter.py --file_name '../iris.data'
 assert_no_stdout
 assert_exit_code 0
+
+run test_plotter python3 ../plotter.py --file_name '../iris255.data'
+assert_in_stdout 'Could not find'
+assert_exit_code 1
