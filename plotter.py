69f093152f06a57ec109d6ec28e378d775c5bf7b
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 04:37:18 +0000

    added scatter and combined plots

--- plotter.py
+++ plotter.py
@@ -10,3 +10,35 @@ params = ['sepal_width', 'sepal_length', 'petal_width', 'petal_length']
 ax1.boxplot(iris[params], labels = params)
 ax1.set_ylabel('cm')
 fig1.savefig('iris_boxplot.png')
+
+# make scatter plot
+fig2, ax2 = plt.subplots()
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name]
+    plt.scatter(iris_subset['petal_length'], iris_subset['petal_width'], label=species_name, s=5)
+ax2.legend()
+ax2.set_xlabel('petal_length (cm)')
+ax2.set_ylabel('petal_width (cm)')
+fig2.savefig('petal_length_v_width_scartter.png')
+
+# make combined plot
+fig, axes = plt.subplots(1, 2)
+fig.set_size_inches(10, 5)
+
+axes[0].boxplot(iris[params], labels=params)
+axes[0].set_ylabel('cm')
+
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name]
+    axes[1].scatter(iris_subset['petal_length'], iris_subset['petal_width'],label=species_name, s=5)
+axes[1].legend()
+axes[1].set_xlabel('petal_length (cm)')
+axes[1].set_ylabel('petal_width (cm)')
+
+for i in range(2):
+    axes[i].spines['top'].set_visible(False)
+    axes[i].spines['right'].set_visible(False)
+    axes[i].spines['bottom'].set_visible(True)
+    axes[i].spines['left'].set_visible(True)
+
+fig.savefig('multi_panel_figure.png')
