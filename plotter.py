2d97d07b90bc4e0eb7a0fc3ac8ffbd71aa4a5773
Kurt <Kurtis.Winkler@Colorado.edu>
2022-10-24 03:25:15 +0000

    updated to make box plot

--- plotter.py
+++ plotter.py
@@ -1,1 +1,12 @@
-# Your code to create majestic plots goes in here!
+import pandas as pd
+import matplotlib.pyplot as plt
+
+iris = pd.read_csv('iris.data', sep=',', header=None)
+iris.columns = ['sepal_width', 'sepal_length', 'petal_width', 'petal_length', 'species']
+
+# make box plot
+fig1, ax1 = plt.subplots()
+params = ['sepal_width', 'sepal_length', 'petal_width', 'petal_length']
+ax1.boxplot(iris[params], labels = params)
+ax1.set_ylabel('cm')
+fig1.savefig('iris_boxplot.png')
